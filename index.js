03650db3569860229c0ac0aebac5ed291f390a74
Christopher Lopes <chrislopes1091@gmail.com>
2018-01-06 02:54:37 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,37 @@
+  function add(a, b ) {
+    return a + b
+  }
+
+  function subtract(a , b) {
+    return a - b
+  }
+
+  function divide(a , b) {
+    return a / b
+  }
+
+  function multiply(a , b) {
+    return a * b
+  }
+
+  function inc(a) {
+    return ++a
+  }
+
+  function dec(a){
+    return --a
+  }
+
+
+  function makeInt(n) {
+    parseInt("n")
+  var parsed = parseInt("n", 10);
+  if makeInt = { return 0 }
+  return parsed ;
+}
+
+console.log(makeint(' 0x2328', 10));
+// expected output: 1500
+
+console.log(roughScale('321', 2));
+// expected output: 0
